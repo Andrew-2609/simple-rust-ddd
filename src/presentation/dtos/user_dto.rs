7e5324d1d57aea04645876d8487ae51c738cdd40
Andrew Monteiro <andrew.agoravai@gmail.com>
2025-05-04 19:45:00 -0300

    test: implement unit tests for user DTOs

--- src/presentation/dtos/user_dto.rs
+++ src/presentation/dtos/user_dto.rs
@@ -10,7 +10,7 @@ pub struct CreateUserDTO {
     pub address: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, PartialEq)]
 pub struct LoadedUserDTO {
     pub id: i32,
     pub name: String,
@@ -31,5 +31,44 @@ impl From<User> for Option<LoadedUserDTO> {
             }),
             ID::New => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::domain::entities::user::User;
+    use crate::domain::value_objects::id::ID;
+    use crate::presentation::dtos::user_dto::LoadedUserDTO;
+
+    #[test]
+    fn from_user_into_optional_loaded_user_dto() {
+        let id = 42;
+        let name = "Andrew";
+        let email = "andrew@email.com";
+        let phone = "+550011111-2222";
+        let address = "Dawn St.";
+
+        let new_user = User::new(
+            name.to_string(),
+            email.to_string(),
+            phone.to_string(),
+            address.to_string(),
+        );
+
+        let loaded_user_dto: Option<LoadedUserDTO> = new_user.clone().into();
+
+        assert!(loaded_user_dto.is_none());
+
+        let mut existing_user = new_user;
+        existing_user.id = ID::Existing(id);
+
+        let loaded_user_dto: Option<LoadedUserDTO> = existing_user.into();
+        let loaded_user_dto = loaded_user_dto.unwrap();
+
+        assert_eq!(loaded_user_dto.id, id);
+        assert_eq!(loaded_user_dto.name, name);
+        assert_eq!(loaded_user_dto.email, email);
+        assert_eq!(loaded_user_dto.phone, phone);
+        assert_eq!(loaded_user_dto.address, address);
     }
 }
